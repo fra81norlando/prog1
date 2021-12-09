076de2e97c197ea80336df2268c3528952e62059
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2021-12-09 11:50:24 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,19 +1,31 @@
-package com.example.array_1;
+package com.example.prova_orlando;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
+
 public class MainActivity extends AppCompatActivity {
-    String nazioni[] = ["italia", "francia", "spagna", "germania"];
+    String nazioni[] = {"italia", "francia", "spagna", "germania"};
     ListView listastati;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listastati = (ListView) findViewById(R.id.lvstati);
-        ArrayAdapter <String> astati = new ArrayAdapter<String>(this,R.layout.activity_main, nazioni);
+        Button btnesegui = (Button) findViewById(R.id.btnEsegui);
+
+        btnesegui.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View v) {
+
+                                         }
+                                     });
+
+                listastati = (ListView) findViewById(R.id.listastati);
+        ArrayAdapter<String> astati = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nazioni);
         listastati.setAdapter(astati);
     }
-    }
+}
