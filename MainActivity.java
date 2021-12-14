6af8bc39da2a9612bb0c8719fe5bdcb768827dac
Francesco Orlando <93184417+fra81norlando@users.noreply.github.com>
2021-12-14 09:52:37 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,11 +1,14 @@
-package com.example.prova_orlando;
+package com.example.myapplication_orlando;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,14 +21,23 @@ public class MainActivity extends AppCompatActivity {
         Button btnesegui = (Button) findViewById(R.id.btnEsegui);
 
         btnesegui.setOnClickListener(new View.OnClickListener() {
-                                         @Override
-                                         public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-                                         }
-                                     });
+            }
+        });
 
-                listastati = (ListView) findViewById(R.id.listastati);
+        listastati = (ListView) findViewById(R.id.listastati);
         ArrayAdapter<String> astati = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, nazioni);
         listastati.setAdapter(astati);
+        listastati.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                String nazioni = (String) listastati.getItemAtPosition(i);
+                Log.d("errori nostri", "posizione" +i);
+                Toast.makeText(getApplicationContext(), nazioni, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
